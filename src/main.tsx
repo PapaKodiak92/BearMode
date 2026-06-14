89ff63f19397c5c6650c966f6b01cb3f7ea1ac8f
PapaKodiak92 <brisendine.joseph92@gmail.com>
2026-06-14 13:07:56 -0500

    Use source dropdown contrast stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '../public/dropdown-fix.css';
+import './dropdown-fix.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
